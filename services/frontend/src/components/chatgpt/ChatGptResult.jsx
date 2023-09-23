17cdcf77abb671c67b3336ef4d16be5d44f46999
Dan Leonard <dcl525@gmail.com>
2023-09-23 08:54:17 +0000

    changes

--- services/frontend/src/components/chatgpt/ChatGptResult.jsx
+++ services/frontend/src/components/chatgpt/ChatGptResult.jsx
@@ -1,36 +1,27 @@
-import { TextField } from '@mui/material';
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { requestType } from '../../api/data/chatGpt';
-import { getUsage } from '../../store/chatgpt/chatGptActions';
-import { ChatGptImageList } from './ChatGptImageList';
+import { TextField } from "@mui/material";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { requestType } from "../../api/data/chatGpt";
+import { getUsage } from "../../store/chatgpt/chatGptActions";
+import { ChatGptImageList } from "./ChatGptImageList";
 
 const ChatGptResult = () => {
   const dispatch = useDispatch();
-  const {
-    selectedRequestType = '',
-    prediction = '',
-    images = [],
-  } = useSelector((x) => x.chatgpt);
 
-  useEffect(() => {
-    dispatch(getUsage());
-  }, []);
+  const { selectedRequestType = "", prediction = "", images = [] } = useSelector((x) => x.chatgpt);
 
   return (
     <>
-      {selectedRequestType === requestType.completion &&
-        prediction && (
-          <TextField
-            multiline
-            label={selectedRequestType}
-            minRows={10}
-            fullWidth
-            value={prediction ?? ''}
-          />
-        )}
-      {selectedRequestType === requestType.image &&
-        images.length > 0 && <ChatGptImageList />}
+      {selectedRequestType === requestType.completion && prediction && (
+        <TextField
+          multiline
+          label={selectedRequestType}
+          minRows={10}
+          fullWidth
+          value={prediction ?? ""}
+        />
+      )}
+      {selectedRequestType === requestType.image && images.length > 0 && <ChatGptImageList />}
     </>
   );
 };
