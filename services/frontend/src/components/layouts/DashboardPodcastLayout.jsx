9f64a5ee79d47eb7bf2519911ae2488c70e5d0ad
Dan Leonard <dcl525@gmail.com>
2024-01-12 23:14:59 +0000

    Merged PR 1714: Update Podcast Service
    Update podcast service

--- services/frontend/src/components/layouts/DashboardPodcastLayout.jsx
+++ services/frontend/src/components/layouts/DashboardPodcastLayout.jsx
@@ -1,12 +1,66 @@
-import { Container, Grid, Typography } from '@mui/material';
+import {
+  Container,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from '@mui/material';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPodcasts } from '../../store/podcasts/podcastActions';
+import Spinner from '../Spinner';
 
 const DashboardPodcastLayout = () => {
-  const { showsLoading = true } = useSelector((x) => x.podcast);
+  const { showsLoading = true, shows = [] } = useSelector(
+    (x) => x.podcast
+  );
+
+  const [selectedShow, setSelectedShow] = React.useState('');
 
   const dispatch = useDispatch();
+
+  const handleSelectShow = (show) => {
+    setSelectedShow(show);
+  };
+
+  const PodcastList = ({ podcasts: shows }) => {
+    return (
+      <List>
+        {shows.map((show) => (
+          <ListItem disablePadding>
+            <ListItemButton
+              onClick={() => handleSelectShow(show)}
+              selected={show.show_title == selectedShow.show_title}>
+              <ListItemText primary={show.show_title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    );
+  };
+
+  const EpisodeList = ({ show }) => {
+    const episodes = show?.episodes || [];
+
+    const handleSelectEpisode = (episode) => {
+      window.open(episode.audio, '_blank');
+    };
+
+    return (
+      <List>
+        {episodes.map((episode) => (
+          <ListItem disablePadding>
+            <ListItemButton
+              onClick={() => handleSelectEpisode(episode)}>
+              <ListItemText primary={episode.episode_title} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    );
+  };
 
   useEffect(() => {
     dispatch(getPodcasts());
@@ -18,16 +72,24 @@ const DashboardPodcastLayout = () => {
         <Grid item xs={12}>
           <Typography variant='h4'>Podcasts</Typography>
         </Grid>
-        <Grid item xs={12}>
+        <Grid item xs={6}>
           <Typography variant='body1'>
-            {showsLoading
-              ? 'Loading...'
-              : 'Podcasts loaded successfully'}
+            {showsLoading ? (
+              <Spinner />
+            ) : (
+              <PodcastList
+                podcasts={shows}
+                onClick={handleSelectShow}
+              />
+            )}
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant='body1'>
+            {selectedShow && <EpisodeList show={selectedShow} />}
           </Typography>
         </Grid>
       </Grid>
-      {/* <FeatureTopMenu /> */}
-      {/* {featuresLoading ? <Spinner /> : <FeatureList />} */}
     </Container>
   );
 };
