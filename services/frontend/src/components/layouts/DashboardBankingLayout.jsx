a1963e330441413eecf3efe22d0c907ffd9e0a68
Dan Leonard <dcl525@gmail.com>
2025-08-17 19:12:16 +0000

    Merged PR 2134: Plaid Sync Console External Link
    External link to Plaid Sync Console

--- services/frontend/src/components/layouts/DashboardBankingLayout.jsx
+++ services/frontend/src/components/layouts/DashboardBankingLayout.jsx
@@ -1,4 +1,13 @@
-import { Grid, Paper, Tab, Tabs } from '@mui/material';
+import {
+  Grid,
+  Paper,
+  Tab,
+  Tabs,
+  Button,
+  Tooltip,
+} from '@mui/material';
+import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getBalances } from '../../store/bank/bankActions';
@@ -39,17 +48,37 @@ const DashboardBankingLayout = () => {
       sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
       <Grid container spacing={2}>
         <Grid item lg={12} xs={12} sm={12}>
-          <Tabs
-            sx={{ marginBottom: '1rem' }}
-            value={selectedTab}
-            onChange={(event, tab) => handleSetSelectedTab(tab)}>
-            <Tab label='Balances' value='balances' />
-            <Tab
-              label='Transactions'
-              value='transactions'
-              disabled={!selectedBankKey}
-            />
-          </Tabs>
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'space-between',
+            }}>
+            <Tabs
+              sx={{ marginBottom: '1rem' }}
+              value={selectedTab}
+              onChange={(event, tab) => handleSetSelectedTab(tab)}>
+              <Tab label='Balances' value='balances' />
+              <Tab
+                label='Transactions'
+                value='transactions'
+                disabled={!selectedBankKey}
+              />
+            </Tabs>
+            <Tooltip title='Open Plaid Sync Console' placement='left'>
+              <Button
+                variant='outlined'
+                color='primary'
+                component='a'
+                href='https://plaid-sync-console.dan-leonard.com'
+                target='_blank'
+                rel='noopener noreferrer'
+                startIcon={<OpenInNewIcon />}
+                sx={{ marginLeft: 2, fontWeight: 500 }}>
+                Console
+              </Button>
+            </Tooltip>
+          </div>
           {selectedTab === 'balances' && <BalanceTab />}
           {selectedTab === 'transactions' && <TransactionsTab />}
         </Grid>
